7d64c921094df07d4cc94b34eb5f8bc6518cf694
dongwlin <dongwlin@foxmail.com>
2025-10-26 02:29:40 +0800

    test: add more episode extraction test cases

--- main_test.go
+++ main_test.go
@@ -18,6 +18,26 @@ func TestExtractEpisode(t *testing.T) {
 			Raw:             "[Lilith-Raws] Kubo-san wa Mob wo Yurusanai - 04 [Baha][WEB-DL][1080p][AVC AAC][CHT][MP4].mp4",
 			ExpectedEpisode: 4,
 		},
+		{
+			Raw:             "[BeanSub&FZSD][Chainsaw_Man][02][GB][1080P][x264_AAC].mp4",
+			ExpectedEpisode: 2,
+		},
+		{
+			Raw:             "S01E07",
+			ExpectedEpisode: 7,
+		},
+		{
+			Raw:             "EP12 - The Final Battle",
+			ExpectedEpisode: 12,
+		},
+		{
+			Raw:             "第03集 - 新的开始",
+			ExpectedEpisode: 3,
+		},
+		{
+			Raw:             "No episode info here",
+			ExpectedEpisode: -1,
+		},
 	}
 
 	for _, tc := range testCases {
